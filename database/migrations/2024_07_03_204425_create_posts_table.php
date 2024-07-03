9c64e0c50022c356d36af9a21895953bcffb28ba
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-03 21:00:21 +0800

    feat: add user_id and body column for posts table.

--- database/migrations/2024_07_03_204425_create_posts_table.php
+++ database/migrations/2024_07_03_204425_create_posts_table.php
@@ -4,16 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id(); // Primary key
+            $table->foreignId('user_id')->constrained(); // Foreign key to users table
+            $table->text('body'); // Post content
+            $table->timestamps(); // created_at and updated_at columns
         });
     }
 
