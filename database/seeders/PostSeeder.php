a717b6795b67a09f1480f10f88ca2a56a46ce5f4
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-03 21:03:53 +0800

    feat: creating 10 Post records.  creating 10 Post records and associating them with a newly created User record.  Changes to be committed: 	modified:   database/seeders/PostSeeder.php

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,10 @@ class PostSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Post::factory()->count(10)->for(User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+        ]))->create();
     }
 }
