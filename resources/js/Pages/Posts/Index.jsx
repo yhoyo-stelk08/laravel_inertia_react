4419564565bbf9fa8591afe8abe9a5c613c344f6
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-03 22:17:56 +0800

    feat: rendering PostResource data that passed to frontend.

--- resources/js/Pages/Posts/Index.jsx
+++ resources/js/Pages/Posts/Index.jsx
@@ -16,7 +16,7 @@ export default function AllPostPage({ auth, posts }) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        {posts.map((post) => {
+                        {posts.data.map((post) => {
                             return (
                                 <div
                                     className="p-6 text-gray-900"
